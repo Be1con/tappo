6695c5e567667a7f5af38665170edcfe98f6c991
Be1con <be1con-tr@live.com>
2017-05-21 01:06:47 +0700

    Set timeout condition (not working, yet)

--- src/pages/play/play.ts
+++ src/pages/play/play.ts
@@ -1,3 +1,4 @@
+//Import Essential Component
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { TimerComponent } from "../components/timer/timer";
@@ -9,13 +10,16 @@ import { RankingPage } from '../ranking/ranking';
 
 //Import Services
 import { LeaderboardService } from '../../services/leaderboard.service';
+
 /**
  * Generated class for the Rule page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-var number=0, Gnumber="Times";
+
+//Set initial variable
+var number = 0, Gnumber = "Times";
 
 @IonicPage()
 @Component({
@@ -86,5 +90,10 @@ export class PlayPage
     redirect()
     {
         this.navCtrl.push(RankingPage);
+    }
+
+    isTimeout()
+    {
+        return this.timeLeft;
     }
 }
